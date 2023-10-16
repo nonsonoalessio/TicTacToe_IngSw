e2cb61c9aad4739600a28ea8b2a088f0fb39e17e
alessio.giura@gmail.com <35069388+nonsonoalessio@users.noreply.github.com>
2023-10-16 10:35:29 +0200

    Aggiunta metodi + attributi BotPlayer per realizzazione detailedClass.puml.

--- src/it/progettiS/entities/BotPlayer.java
+++ src/it/progettiS/entities/BotPlayer.java
@@ -5,9 +5,30 @@ public class BotPlayer extends Player {
         super(mark);
     }
 
+    private Move m;
+
     @Override
     public Move makeMove(Board board){
-        Move m;
-        return m;
+        if(winningMoveAvailable(board, this.mark))
+            return m;
+        if(blockingMoveAvailable(board, this.mark))
+            return m;
+        if(strategicMoveAvailable(board, this.mark))
+            return m;
+        else return randomMove(board, mark);
     }
+
+
+    private boolean winningMoveAvailable(Board b, char mark){
+        // check orizzontale
+        // check verticale
+        // check diagonale
+    }
+    private boolean blockingMoveAvailable(Board b, char mark){
+        // check orizzontale
+        // check verticale
+        // check diagonale
+    }
+    private boolean strategicMoveAvailable(Board b, char mark){}
+    private Move randomMove(Board b, char mark){}
 }
